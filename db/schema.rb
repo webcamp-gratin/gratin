290b4a470d5c4ffcc07f169671c3f27aaccf1828
EC2 Default User <ec2-user@ip-172-31-9-166.ec2.internal>
2022-06-16 06:49:24 +0000

    customer/addressモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_024341) do
+ActiveRecord::Schema.define(version: 2022_06_16_050753) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name"
+    t.string "address"
+    t.string "postcode"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
