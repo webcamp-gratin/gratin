df84d29fb87fecaf65c4dee4acaa2eadb793ebea
EC2 Default User <ec2-user@ip-172-31-92-105.ec2.internal>
2022-06-18 05:21:53 +0000

    エラー解決3

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,8 +6,11 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save!
-    redirect_to admin_item_path(@item)
+    if　@item.save!
+      redirect_to admin_item_path(@item)
+    else
+      render index
+    end
   end
 
   def index
