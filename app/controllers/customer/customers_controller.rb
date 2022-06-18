7865bd5363ae678a38b0057c3a4625ceb8134773
EC2 Default User <ec2-user@ip-172-31-9-166.ec2.internal>
2022-06-18 09:55:19 +0000

    customerのmigrateとcontroller作成

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,2 +1,22 @@
 class Customer::CustomersController < ApplicationController
+
+  def show
+    @customer == current_customer
+  end
+
+  def edit
+    @customer == current_customer
+  end
+
+  def update
+    @customer == current_customer
+    @customer.update(customer_params)
+    redirect_to customer_path
+
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:name, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :is_delete)
+  end
 end
--- db/migrate/20220616024341_devise_create_customers.rb
+++ db/migrate/20220616024341_devise_create_customers.rb
@@ -6,21 +6,13 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
       t.string :last_name
-
       t.string :first_name
-
       t.string :last_name_kana
-
       t.string :first_name_kana
-
       t.string :postcode
-
       t.string :address
-
       t.string :phone_number
-
       t.boolean :is_deleted, default: true, null: false
 
       ## Recoverable
