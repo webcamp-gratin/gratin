d90efd36c1edd9d717b346a980065abc0403f423
EC2 Default User <ec2-user@ip-172-31-92-105.ec2.internal>
2022-06-20 10:25:55 +0000

    修正

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -21,7 +21,7 @@ class Customer::CustomersController < ApplicationController
 
   def withdraw
     @customer = Customer.find(params[:id])
-    @customer.update(is_deleted: false)
+    @customer.update(is_deleted: true)
     reset_session
     flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
