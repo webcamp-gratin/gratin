468f9c61c838c6b6d70890555122087e15a01136
EC2 Default User <ec2-user@ip-172-31-34-197.ec2.internal>
2022-06-21 06:17:32 +0000

    cart_item_fix

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -2,11 +2,24 @@ class Customer::CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.item_id = params[:item_id]
-    @cart_item.save
-    redirect_to cart_items_path
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @update_cart_item =  current_customer.cart_items.find_by(item_id: @cart_item.item_id)
+    if  @update_cart_item.present?
+        @update_cart_item.amount += params[:cart_item][:amount].to_i
+        #binding.pry
+        @update_cart_item.save
+        redirect_to cart_items_path
+    else
+        if @cart_item.save
+          flash[:notice] = "カートに追加しました"
+          redirect_to cart_items_path
+        else
+          @item = Item.find(params[:cart_item][:item_id])
+          @cart_item = CartItem.new
+          flash[:alert] = "個数を選択してください"
+          render "customer/items/show"
+        end
+    end
   end
 
   def index
@@ -16,7 +29,7 @@ class Customer::CartItemsController < ApplicationController
 
   def update
     @cart_item = CartItem.find(params[:id])
-    @cart_item.update(cart_item_params)
+    @cart_item.update(amount: params[:cart_item][:amount].to_i)
     redirect_to cart_items_path
   end
 
