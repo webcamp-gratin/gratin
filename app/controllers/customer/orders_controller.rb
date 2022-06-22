0b622b2af9417c378cc5e93a0bc84f7d7d41e818
EC2 Default User <ec2-user@ip-172-31-9-166.ec2.internal>
2022-06-22 08:50:29 +0000

    confirm/complete完成git add -Agit add -A

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -19,9 +19,8 @@ class Customer::OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
-  #newから送られてきたパラメータの取得
   def confirm
-    @order =Order.new(order_params)
+    @order = Order.new(order_params)
     if params[:order][:address_option] == "0"
       @order.postcode = current_customer.postcode
       @order.address = current_customer.address
@@ -33,17 +32,24 @@ class Customer::OrdersController < ApplicationController
       @order.name = @address.name
     elsif params[:order][:address_option] == "2"
     end
-
-
     @cart_items = current_customer.cart_items
     @total_price = @cart_items.sum{|cart_item|cart_item.item.no_tax * cart_item.amount * 1.1}
-
-
+    @order.save
+    @cart_items = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+    @ordered_items = @order.ordered_items.new
+    @ordered_items.item_id = cart_item.item.id
+    @ordered_items.price = cart_item.item.no_tax
+    @ordered_items.amount = cart_item.amount
+    @ordered_items.save
+    end
+    current_customer.cart_items.destroy_all
+    redirect_to orders_complete_path
   end
 
-  private
+private
   def order_params
-    params.require(:order).permit(:customer_id, :postage, :subtotal, :payment_method, :name, :address, :postcode, :status)
+  params.require(:order).permit(:customer_id, :postage, :subtotal, :payment_method, :name, :address, :postcode, :status)
   end
 
 end
