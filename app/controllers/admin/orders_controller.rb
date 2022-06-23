0a99a7bca6b8f5b3c7526511b21494879e5bf37b
EC2 Default User <ec2-user@ip-172-31-9-166.ec2.internal>
2022-06-23 05:59:13 +0000

    test

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,7 @@
 class Admin::OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+    @ordered_items = @order.ordered_items
+    @total_price = @ordered_items.sum{|ordered_item|ordered_item.item.no_tax * ordered_item.amount * 1.1}
+  end
 end
