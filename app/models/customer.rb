565163fe5bbd1f5a98ce39de640b9f2e49732846
EC2 Default User <ec2-user@ip-172-31-34-197.ec2.internal>
2022-06-24 07:21:13 +0000

    c

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,6 +7,8 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
 
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, presence: true
+
   def active_for_authentication?
     super && (is_deleted == false)
   end
